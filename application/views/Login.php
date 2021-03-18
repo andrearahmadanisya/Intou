191f5bd32b0c5853471270b77324f1560b9528ad
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-03-18 07:07:41 +0700

    Update Login.php

--- application/views/Login.php
+++ application/views/Login.php
@@ -1,42 +1,39 @@
-<div class="container">
+<html>
 
-	<!-- Outer Row -->
-	<div class="row justify-content-center">
+<head>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="<?php base_url(); ?>assets/css/login.css" type="text/css" />
+	<!------ Include the above in your HEAD tag ---------->
+</head>
 
-		<div class="col-lg-7">
-
-			<div class="card o-hidden border-0 shadow-lg my-5">
-				<div class="card-body p-0">
-					<!-- Nested Row within Card Body -->
-					<div class="row">
-						<div class="col-lg">
-							<div class="p-5">
-								<div class="text-center">
-									<h1 class="h4 text-gray-900 mb-4">Wellcome to IntoU</h1>
-								</div>
-
-								<form action="<?= site_url('LoginController') ?>" method="post" accept-charset="utf-8">
-									<div class="form-group">
-										<label class="label control-label">Username</label>
-										<input type="username" class="form-control" name="username" placeholder="Username">
-									</div>
-
-									<div class="form-group">
-										<label class="label control-label">Password</label>
-										<input type="password" class="form-control" name="password" placeholder="Password">
-									</div>
-
-									<button type="submit" class="btn btn-info">Login</button>
-								</form>
-								<hr>
+<body class="login">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="row" style="margin-top:70px">
+					<div class="col-md-6">
+						<ul class="text-center"><a>USER LOGIN</a></ul>
+						<form action="<?= site_url('LoginController') ?>" method="post" accept-charset="utf-8">
+							<div class="form-group">
+								<label class="label control-label">Username</label>
+								<input type="username" class="form-control" name="username" placeholder="Username">
 							</div>
-						</div>
+
+							<div class="form-group">
+								<label class="label control-label">Password</label>
+								<input type="password" class="form-control" name="password" placeholder="Password">
+							</div>
+
+							<button type="submit" class="btn btn-info">Login</button>
+						</form>
 					</div>
 				</div>
 			</div>
-
 		</div>
-
 	</div>
+</body>
 
-</div>
+</html>
