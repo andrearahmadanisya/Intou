8f80df228a409dd87b7af0279b4f20bedcb8cfbd
sindifatikas <61447377+sindifatikas@users.noreply.github.com>
2021-03-22 20:06:11 +0700

    Update Login.php

--- application/views/Login.php
+++ application/views/Login.php
@@ -1,39 +1,25 @@
+<!DOCTYPE html>
 <html>
-
 <head>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="<?php base_url(); ?>assets/css/login.css" type="text/css" />
-	<!------ Include the above in your HEAD tag ---------->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+	<link href="assets/css/login.css" rel="stylesheet">
+	<title>Login | INTOU</title>
 </head>
-
-<body class="login">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-2"></div>
-			<div class="col-md-8">
-				<div class="row" style="margin-top:70px">
-					<div class="col-md-6">
-						<ul class="text-center"><a>USER LOGIN</a></ul>
-						<form action="<?= site_url('LoginController') ?>" method="post" accept-charset="utf-8">
-							<div class="form-group">
-								<label class="label control-label">Username</label>
-								<input type="username" class="form-control" name="username" placeholder="Username">
-							</div>
-
-							<div class="form-group">
-								<label class="label control-label">Password</label>
-								<input type="password" class="form-control" name="password" placeholder="Password">
-							</div>
-
-							<button type="submit" class="btn btn-info">Login</button>
-						</form>
-					</div>
-				</div>
-			</div>
+<body>
+	<div class="pertama" style="background-image: url('assets/images/bg2.png');">
+    <div class="gambar1">
+			<img src="assets/images/login1.png">
+    </div>
+    
+		<div class= "loginbox">
+			<h1>USER LOGIN</h1>
+				<form action="" method="post" class="login__form">
+				<input type="text" name="username" placeholder="Username">
+				<input type="password" name="password" placeholder="Password">
+				<input type="submit" name="login" value="Login">
+			</form>
 		</div>
 	</div>
 </body>
-
 </html>
