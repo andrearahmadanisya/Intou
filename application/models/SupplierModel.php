f60cf80a77900be104ef3db28ac61ac8d5c60878
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-03-23 00:04:39 +0700

    search

--- application/models/SupplierModel.php
+++ application/models/SupplierModel.php
@@ -30,4 +30,16 @@ class SupplierModel extends CI_Model
 	{
 		return $this->db->delete('supplier', ['idsupplier' => $idsupplier]);
 	}
+	
+	public function get_keyword($keyword)
+	{
+		$this->db->select('*');
+		$this->db->from('supplier');
+		$this->db->like('Namasupplier', $keyword);
+		$this->db->or_like('alamatsupplier', $keyword);
+		$this->db->or_like('contactsupplier', $keyword);
+		$this->db->or_like('emailsupplier', $keyword);
+		$this->db->or_like('kotasupplier', $keyword);
+		return $this->db->get()->result_array();
+	}
 }
