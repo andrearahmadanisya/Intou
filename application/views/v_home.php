8920dc705e02b3e62a7b272ee9e2f638256b2688
sindifatikas <61447377+sindifatikas@users.noreply.github.com>
2021-03-27 20:13:49 +0700

    Update v_home.php

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+  <!-- ini manggil css -->
+  <link rel="stylesheet" type="text/css" href="assets/css/home.css"/>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,8 +17,6 @@
   <!-- Bootstrap CSS -->
   <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- styles template -->
-  <link href="assets/css/home.css" rel="stylesheet">
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
