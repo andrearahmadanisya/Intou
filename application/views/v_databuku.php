c44daa23c5c96e06978db78a869065a03c52eff9
sindifatikas <61447377+sindifatikas@users.noreply.github.com>
2021-03-27 20:10:31 +0700

    Update v_databuku.php

--- application/views/v_databuku.php
+++ application/views/v_databuku.php
@@ -2,6 +2,9 @@
 <html lang="en">
 <head>
 
+  <!-- ini manggil css -->
+  <link rel="stylesheet" type="text/css" href="assets/css/databuku.css"/>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
