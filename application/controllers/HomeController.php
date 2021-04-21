0a1caba7c68ecb7c99292cf9df3da81dd8697315
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-04-21 14:56:09 +0700

    Update HomeController.php

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -1,31 +1,121 @@
 <?php
 
-class HomeController extends CI_Controller
+class BukuController extends CI_Controller
 {
 	public function __construct()
 	{
 		parent::__construct();
-		// load HomeModel
-		//$this->load->model('HomeModel');
+		// load BukuMode
+		$this->load->model('BukuModel');
+		$this->load->model('HistoryModel');
+		$this->load->library('form_validation');
+		$this->load->helper('date');
 	}
 
 	public function index()
 	{
-		$data['judul'] = 'Home';
+		$data['judul'] = 'Data Buku';
 		$data['user'] = $this->session->userdata('user');
 
-		$this->load->view('v_home', $data);
+		$data['buku'] = $this->BukuModel->getAllBuku();
+		$this->load->view('v_databuku', $data);
 	}
 
-	public function login()
+	public function addBuku()
 	{
-		$data['judul'] = 'Login';
-		$this->load->view('Login');
+		$this->form_validation->set_rules('judul', 'judul', 'required');
+		$this->form_validation->set_rules('category', 'category', 'required');
+		$this->form_validation->set_rules('penulis', 'penulis', 'required');
+		$this->form_validation->set_rules('total', 'total', 'required');
+		$this->form_validation->set_rules('tglmasuk', 'tglmasuk', 'required');
+		$this->form_validation->set_rules('hargajual', 'hargajual', 'required');
+		$this->form_validation->set_rules('hargabeli', 'hargabeli', 'required');
+
+		if ($this->form_validation->run() == FALSE) {
+			$data['judul'] = 'Data Buku';
+			$data['buku'] = $this->BukuModel->getAllBuku();
+
+			$this->load->view('v_databuku');
+		} else {
+			date_default_timezone_set('Asia/Jakarta');
+			$now = date('Y-m-d H:i:s');
+			$add = [
+				"idbuku" => '',
+				"judul" => $this->input->post('judul', true),
+				"category" => $this->input->post('category', true),
+				"penulis" => $this->input->post('penulis', true),
+				"total" => $this->input->post('total', true),
+				"tglmasuk" => $this->input->post('tglmasuk', 'NOW()'),
+
+				"hargajual" => $this->input->post('hargajual', true),
+				"hargabeli" => $this->input->post('hargabeli', true)
+			];
+			$this->BukuModel->addBuku($add);
+			// load Bukucon
+
+		}
+		redirect('BukuController');
 	}
 
-	public function logout()
+	public function delete($id)
 	{
-		$this->session->sess_destroy();
-		redirect('LoginController');
+		$buku = $this->BukuModel->getById($id);
+		$this->HistoryModel->addHistory($buku);
+		$this->BukuModel->deleteBuku($id);
+
+		// $this->BukuModel->deleteBuku($id);
+		redirect('BukuController');
+	}
+
+	public function cari()
+	{
+		$data['judul'] = 'Data Buku';
+		$data['buku'] = $this->BukuModel->getAllBuku();
+
+		if ($this->input->post('submit')) {
+			$data['keyword'] = $this->input->post('keyword');
+		} else {
+			$data['keyword'] = null;
+		}
+		redirect('BukuController');
+	}
+	public function update($id)
+	{
+		$data['judul'] = 'Buku';
+		$data['buku'] = $this->BukuModel->getAllBuku();
+
+		//from library form_validation
+		$this->form_validation->set_rules('judul', 'judul', 'required');
+		$this->form_validation->set_rules('category', 'category', 'required');
+		$this->form_validation->set_rules('penulis', 'penulis', 'required');
+		$this->form_validation->set_rules('total', 'total', 'required');
+		$this->form_validation->set_rules('tglmasuk', 'tglmasuk', 'required');
+		$this->form_validation->set_rules('hargajual', 'hargajual', 'required');
+		$this->form_validation->set_rules('hargabeli', 'hargabeli', 'required');
+
+		if ($this->form_validation->run() == false) {
+
+			$this->load->view('v_databuku', $data);
+		} else {
+			$update = [
+				"idbuku" => $id,
+				"judul" => $this->input->post('judul', true),
+				"category" => $this->input->post('category', true),
+				"penulis" => $this->input->post('penulis', true),
+				"total" => $this->input->post('total', true),
+				"tglmasuk" => $this->input->post('tglmasuk', true),
+				"hargajual" => $this->input->post('hargajual', true),
+				"hargabeli" => $this->input->post('hargabeli', true)
+			];
+			$this->BukuModel->updateBuku($id, $update);
+			redirect('BukuController');
+		}
+	}
+
+	public function SearchBuku()
+	{
+		$keyword = $this->input->post('keyword');
+		$data['buku'] = $this->BukuModel->get_keyword($keyword);
+		$this->load->view('v_databuku', $data);
 	}
 }
