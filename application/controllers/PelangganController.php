6ae7eb73d1cea11f01604112a58a39b8e78c7b38
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-05-04 10:49:17 +0700

    Update PelangganController.php

--- application/controllers/PelangganController.php
+++ application/controllers/PelangganController.php
@@ -4,6 +4,7 @@ class PelangganController extends CI_Controller
 {
 	public function __construct()
 	{
+		// load pelanggan model sebagai parent
 		parent::__construct();
 		$this->load->model('PelangganModel');
 		$this->load->library('form_validation');
@@ -11,23 +12,28 @@ class PelangganController extends CI_Controller
 
 	public function index()
 	{
-		$data['judul'] = 'Data Pelanggan';
+		$data['title'] = 'Data Pelanggan';
+		//mengambil session dari data user login 
 		$data['user'] = $this->session->userdata('user');
-
+		// mengambbil data pelanggan yang ada pada db 
 		$data['pelanggan'] = $this->PelangganModel->getAllPelanggan();
+		//load v_datapelanggan dari view
 		$this->load->view('v_datapelanggan', $data);
 	}
 
+	// menambahkan data pelanggan pada db
 	public function addPelanggan()
 	{
+		// membuat form validation untuk input sesuai dengan kolom yang ada pada db
 		$this->form_validation->set_rules('namapelanggan', 'namapelanggan', 'required');
 		$this->form_validation->set_rules('alamatpelanggan', 'alamatpelanggan', 'required');
 		$this->form_validation->set_rules('kotapelanggan', 'kotapelanggan', 'required');
 		$this->form_validation->set_rules('emailpelanggan', 'emailpelanggan', 'required');
 		$this->form_validation->set_rules('contactpelanggan', 'contactpelanggan', 'required');
 
+		// dicek terlebih dahulu kalau false akan kembali ke view v_datapelanggan, else akan menampilkan form yag akan diinputkan
 		if ($this->form_validation->run() == FALSE) {
-			$data['judul'] = 'Data Pelanggan';
+			$data['title'] = 'Data Pelanggan';
 			$data['pelanggan'] = $this->PelangganModel->getAllPelanggan();
 
 			$this->load->view('v_datapelanggan');
@@ -45,15 +51,17 @@ class PelangganController extends CI_Controller
 		redirect('PelangganController');
 	}
 
+	// menghapus data pelanggan pada db 
 	public function delete($id)
 	{
 		$this->PelangganModel->deletePelanggan($id);
 		redirect('PelangganController');
 	}
 
+	// meng-edit data pelanggan 
 	public function update($id)
 	{
-		$data['judul'] = 'Data Pelanggan';
+		$data['title'] = 'Data Pelanggan';
 		$data['pelanggan'] = $this->PelangganModel->getAllPelanggan();
 
 		//from library form_validation
@@ -68,7 +76,7 @@ class PelangganController extends CI_Controller
 			$this->load->view('v_datapelanggan', $data);
 		} else {
 			$update = [
-				"idpelanggan" => $id,
+				"idpelanggan" => $id, //diambil sesuai idpelanggan yang ingin di edit
 				"namapelanggan" => $this->input->post('namapelanggan', true),
 				"alamatpelanggan" => $this->input->post('alamatpelanggan', true),
 				"kotapelanggan" => $this->input->post('kotapelanggan', true),
@@ -80,10 +88,10 @@ class PelangganController extends CI_Controller
 		}
 	}
 
-	public function SearchPelanggan()
-	{
-		$keyword = $this->input->post('keyword');
-		$data['pelanggan'] = $this->PelangganModel->get_keyword($keyword);
-		$this->load->view('v_datapelanggan', $data);
-	}
+	// public function SearchPelanggan()
+	// {
+	// 	$keyword = $this->input->post('keyword');
+	// 	$data['pelanggan'] = $this->PelangganModel->get_keyword($keyword);
+	// 	$this->load->view('v_datapelanggan', $data);
+	// }
 }
