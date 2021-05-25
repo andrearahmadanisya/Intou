cfe68d771b4812c7d7e0829a8c3e3702a2e29cdb
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-05-25 20:03:16 +0700

    Update add.php

--- application/views/buku/add.php
+++ application/views/buku/add.php
@@ -75,15 +75,14 @@
                         <?= form_error('hargajual', '<small class="text-danger">', '</small>'); ?>
                     </div>
                 </div>
-            </div>
-            <div class="row form-group">
-                <div class="col-md-9 offset-md-3">
-                    <button type="submit" class="btn" style="background-color: #315c9a; color: white;">Simpan</button>
-                    <button type="reset" class="btn btn-secondary">Reset</bu>
+                <div class="row form-group">
+                    <div class="col-md-9 offset-md-3">
+                        <button type="submit" class="btn" style="background-color: #315c9a; color: white;">Simpan</button>
+                        <button type="reset" class="btn btn-secondary">Reset</bu>
+                    </div>
                 </div>
+                <?= form_close(); ?>
             </div>
-            <?= form_close(); ?>
         </div>
     </div>
-</div>
 </div>
