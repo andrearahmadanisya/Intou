3f930491e37f238bd39b7b8d6739706deb566001
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-05-25 16:13:32 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,14 +23,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< Updated upstream
-$config['base_url'] = 'http://localhost/Intou-master';
-
-=======
 $config['base_url'] = "http://" . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= preg_replace('@/+$@', '', dirname($_SERVER['SCRIPT_NAME'])) . '/';
 $config['fonts_path'] = 'system/fonts/';
->>>>>>> Stashed changes
 /*
 |--------------------------------------------------------------------------
 | Index File
