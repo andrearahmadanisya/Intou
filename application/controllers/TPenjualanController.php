037ad0584b3db1a7d6d2fa295b9ce939d7c7d947
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-05-25 19:53:32 +0700

    Update TPenjualanController.php

--- application/controllers/TPenjualanController.php
+++ application/controllers/TPenjualanController.php
@@ -1,96 +1,3 @@
-<<<<<<< Updated upstream
-<?php
-
-class TPenjualanController extends CI_Controller
-{
-    public function __construct()
-    {
-        parent::__construct();
-        // load BukuMode
-        $this->load->model('TPenjualanModel');
-        $this->load->library('form_validation');
-        $this->load->helper('date');
-    }
-
-    public function index()
-    {
-        $data['user'] = $this->session->userdata('user');
-
-        $data['transaksipjl'] = $this->TPenjualanModel->getAllPenjualan();
-        $this->load->view('v_transaksipenjualan', $data);
-    }
-
-    public function addPenjualan()
-    {
-        $this->form_validation->set_rules('judulbuku', 'judulbuku', 'required');
-        $this->form_validation->set_rules('qty', 'qty', 'required');
-        $this->form_validation->set_rules('biayakirim', 'biayakirim', 'required');
-        $this->form_validation->set_rules('hargapcs', 'hargapcs', 'required');
-        $this->form_validation->set_rules('hargatotal', 'hargatotal', 'required');
-        $this->form_validation->set_rules('tgltransaksi', 'tgltransaksi', 'required');
-
-        if ($this->form_validation->run() == FALSE) {
-            $data['judul'] = 'Data';
-            $data['transaksipjl'] = $this->TPenjualanModel->getAllPenjualan();
-
-            $this->load->view('v_transaksipenjualan');
-        } else {
-            date_default_timezone_set('Asia/Jakarta');
-            $now = date(DATE_RSS, time());
-            $format = "Y-m-d H:i a";
-
-            $add = [
-                "idpenjualan" => '',
-                "judulbuku" => $this->input->post('judulbuku', true),
-                "qty" => $this->input->post('qty', true),
-                "hargapcs" => $this->input->post('hargapcs', true),
-                "biayakirim" => $this->input->post('biayakirim', true),
-                "hargatotal" => $this->input->post('hargatotal', true),
-                "tgltransaksi" => date($format)
-            ];
-            $this->TPenjualanModel->addPenjualan($add);
-        }
-        redirect('TPenjualanController');
-    }
-
-    public function update($id)
-    {
-        $data['judul'] = 'Buku';
-        $data['transaksipjl'] = $this->TPenjualanModel->getAllPenjualan();
-        //from library form_validation
-        $this->form_validation->set_rules('judulbuku', 'judulbuku', 'required');
-        $this->form_validation->set_rules('qty', 'qty', 'required');
-        $this->form_validation->set_rules('hargapcs', 'hargapcs', 'required');
-        $this->form_validation->set_rules('biayakirim', 'biayakirim', 'required');
-        $this->form_validation->set_rules('hargatotal', 'hargatotal', 'required');
-        $this->form_validation->set_rules('tgltransaksi', 'tgltransaksi', 'required');
-
-        if ($this->form_validation->run() == false) {
-
-            $this->load->view('v_transaksipenjualan', $data);
-        } else {
-            date_default_timezone_set('Asia/Jakarta');
-            $format = "Y-m-d H:i a";
-            $update = [
-                "idbuku" => $id,
-                "judulbuku" => $this->input->post('judulbuku', true),
-                "qty" => $this->input->post('qty', true),
-                "hargatotal" => $this->input->post('hargatotal', true),
-                "hargapcs" => $this->input->post('hargapcs', true),
-                "biayakirim" => $this->input->post('biayakirim', true),
-                "tgltransaksi" => date($format)
-            ];
-            $this->TPenjualanModel->updatepenjualan($id, $update);
-            redirect('TPenjualanController');
-        }
-    }
-    public function delete($id)
-    {
-        $this->TPenjualanModel->deletepenjualan($id);
-        redirect('TPenjualanController');
-    }
-}
-=======
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
@@ -118,19 +25,6 @@ class TPenjualanController extends CI_Controller
         $this->form_validation->set_rules('id_buku', 'buku', 'required');
         $this->form_validation->set_rules('idpelanggan', 'Pelanggan', 'required');
         $this->form_validation->set_rules('qty', 'jumlah keluar', 'required|trim|numeric|greater_than[0]');
-
-        // $input = $this->input->post('id_buku', true);
-        // $stok = $this->admin->get('buku', ['idbuku' => $input])['stok'];
-        // $stok_valid = $stok + 1;
-
-        // $this->form_validation->set_rules(
-        //     'qty',
-        //     'Jumlah Keluar',
-        //     "required|trim|numeric|greater_than[0]|less_than[{$stok_valid}]",
-        //     [
-        //         'less_than' => "Jumlah Keluar tidak boleh lebih dari {$stok}"
-        //     ]
-        // );
     }
 
     public function add()
@@ -141,8 +35,7 @@ class TPenjualanController extends CI_Controller
             $data['pelanggan'] = $this->admin->get('pelanggan');
             $data['buku'] = $this->admin->get('buku', null, ['stok >' => 0]);
 
-            // Mendapatkan dan men-generate kode transaksi barang keluar
-            // $kode = 'TPJL-' . date('ymd');
+            // Mendapatkan dan men-generate kode transaksi 
             $kode_terakhir = $this->admin->getMax('transaksipjl', 'idpenjualan');
             $kode_tambah = substr($kode_terakhir, -4, 4);
             $kode_tambah++;
@@ -201,4 +94,3 @@ class TPenjualanController extends CI_Controller
         redirect('TPenjualanController');
     }
 }
->>>>>>> Stashed changes
