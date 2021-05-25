d83b36551103bbc6d0bcb8aafb85e1ae5fc0ad89
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2021-05-25 16:47:02 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,11 +1,11 @@
 <!-- Outer Row -->
-<div class="row justify-content-center mt-5 pt-lg-5">
+<div class="row justify-content-center pt-lg-5">
     <div class="card o-hidden border-0 shadow-lg">
         <div class="card-body p-lg-5">
             <!-- Nested Row within Card Body -->
             <div class="row">
-                <div class="col-md-6">
-                    <div class="p-5">
+                <div class="col-md">
+                    <div class="p">
                         <div class="text-center mb-4">
                             <div class="col-lg-6 d-none d-lg-block">
                                 <img src="assets/img/logo.png">
@@ -21,7 +21,7 @@
                             <input type="password" name="password" class="form-control form-control-user" placeholder="Password">
                             <?= form_error('password', '<small class="text-danger">', '</small>'); ?>
                         </div>
-                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                        <button type="submit" class="btn btn-user btn-block" style="background-color:#6C7C94; color:white;">
                             Login
                         </button>
                         <?= form_close(); ?>
